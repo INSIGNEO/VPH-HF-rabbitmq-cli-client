95120b89a1572c35ecdf55a828e507c38cafb1e7
Simone Bna <s.bna@scsitaly.com>
2017-02-03 15:23:11 +0100

    uncommented part of the setup code

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# USAGE: bash setup.sh -p port -u user -w password -h host -v vhost
+# run this script to setup the Rabbitmq cli client
 
 while [[ $# -gt 1 ]]
 do
@@ -29,6 +31,7 @@ esac
 shift # past argument or value
 done
 
+# configure the rabbitmq.profile
 echo AMQP_HOST     = "${AMQP_HOST}"
 echo AMQP_VHOST    = "${AMQP_VHOST}"
 echo AMQP_PORT     = "${AMQP_PORT}"
@@ -40,15 +43,15 @@ sed -e "s/\${amqp_host}/`echo ${AMQP_HOST}`/" -e "s/\${amqp_vhost}/`echo ${AMQP_
     -e "s/\${amqp_password}/`echo ${AMQP_PASSWORD}`/" rabbitmq.profile.in | tee rabbitmq.profile 
 
 #build rabbitmq-c
-# cd rabbitmq-c
-# mkdir -p build
-# cd build
-# cmake ..
-# cmake --build .
-# cd ../../
+cd rabbitmq-c
+mkdir -p build
+cd build
+cmake ..
+cmake --build .
+cd ../../
 
 # build amqptools
-# export AMQPTOOLS_RABBITHOME=$PWD/rabbitmq-c
-# cd amqptools
-# make
-# cd ..
+export AMQPTOOLS_RABBITHOME=$PWD/rabbitmq-c
+cd amqptools
+make
+cd ..
